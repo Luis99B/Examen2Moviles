cf6ab5a05f9b0654a5e7138c3fa4cb94cff65c10
Luis99B <30879359+Luis99B@users.noreply.github.com>
2021-04-28 14:17:28 -0500

    Add files via upload

--- app/src/main/java/mx/itesm/luisbodart_gerareyes/MainActivity.java
+++ app/src/main/java/mx/itesm/luisbodart_gerareyes/MainActivity.java
@@ -1,14 +1,62 @@
 package mx.itesm.luisbodart_gerareyes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG_FRAGMENT = "active";
+    private JuegoFragment juego;
+    private PeliculaFragment peli;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        juego = JuegoFragment.newInstance("par1","par2");
+        peli = new PeliculaFragment();
+    }
+
+    private void setActiveFragment(Fragment nvo) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        transaction.add(R.id.container, nvo, TAG_FRAGMENT);
+        transaction.commit();
+    }
+    private void removeActiveFragment() {
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentByTag(TAG_FRAGMENT);
+        if (fragment != null){
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.remove(fragment);
+            transaction.commit();
+        }
+    }
+
+    public void swapToJuegoFragment(View v) {
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentByTag(TAG_FRAGMENT);
+        removeActiveFragment();
+
+        if(fragment != juego) {
+            setActiveFragment(juego);
+        }
+    }
+
+    public void swapToPeliculaFragment(View v) {
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentByTag(TAG_FRAGMENT);
+        removeActiveFragment();
+
+        if(fragment != peli) {
+            setActiveFragment(peli);
+        }
     }
 }
